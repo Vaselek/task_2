fc8106ef00fde48ede09ad0e66f62a476a4b7e7a
Vaselek <burzhuas@gmail.com>
2019-03-14 17:36:20 +0600

    Update for lesson 75, api

--- server.js
+++ server.js
@@ -2,31 +2,25 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const Vigenere = require('caesar-salad').Vigenere;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hi, go to /encode or /decode');
+app.post('/encode', (req, res) => {
+  const {message, password} = req.body;
+  const encodedPassword = Vigenere.Cipher(password).crypt(message);
+  res.send({encoded: encodedPassword});
 });
 
-app.get('/encode', (req, res) => {
-  res.send('Hi, add slash and some word to encode');
-});
-
-app.get('/decode', (req, res) => {
-  res.send('Hi, add slash and some word to decode');
-});
-
-app.get('/encode/:password', (req, res) => {
-  const encodedPassword = Vigenere.Cipher('password').crypt(req.params.password);
-  res.send(encodedPassword);
-});
-
-app.get('/decode/:password', (req, res) => {
-  const decodedPassword = Vigenere.Decipher('password').crypt(req.params.password);
-  res.send(decodedPassword);
+app.post('/decode', (req, res) => {
+  const {message, password} = req.body;
+  const decodedPassword = Vigenere.Decipher(password).crypt(message);
+  res.send({decoded: decodedPassword});
 });
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
 })
-
